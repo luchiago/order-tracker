bdae1690679c99f748b3d39b726f74523a5419a8
Lucas Hiago <lucashiago63@gmail.com>
2023-07-11 16:42:31 -0300

    feat: add scheduler

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import os
 from crawler import main
 from db.database import initialize
 import logging
+import schedule
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
 
 logging.basicConfig(
     filename='logs.log',
@@ -11,5 +17,21 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-initialize()
-main()
+IN_DOCKER = bool(int(os.getenv('IN_DOCKER')))
+
+def run():
+    initialize()
+    main()
+
+def run_scheduled():
+    schedule.every().day.at('10:00', 'America/Sao_Paulo').do(run)
+    schedule.every().day.at('18:00', 'America/Sao_Paulo').do(run)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if IN_DOCKER:
+    run_scheduled()
+else:
+    run()
