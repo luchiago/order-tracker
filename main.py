ec23145569f178dcc29119023c57f7f532a09549
Lucas Hiago <lucashiago63@gmail.com>
2023-07-10 22:08:34 -0300

    feat: add logging

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from crawler import main
+from db.database import initialize
+import logging
 
+logging.basicConfig(
+    filename='logs.log',
+    format='%(asctime)s %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    filemode='w',
+    encoding='utf-8',
+    level=logging.INFO,
+)
+
+initialize()
 main()
